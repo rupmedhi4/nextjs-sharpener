68f2a1a1f0bfa91ac1cc9db842b3268893f18394
Rup medhi <rupmmedhi@gmail.com>
2023-09-06 23:42:49 +0530

    dynamically show about us page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,22 @@
+import Link from "next/link"
 
 export default function page() {
+  const details = [
+    { id: 1, name: 'Yash', role: 'Senior Developer' },
+    { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+    { id: 3, name: 'Suresh', role: 'Frontend Developer' }
+  ]
   return (
-    <div>
-      <h1>this is about page </h1>
-    </div>
+    <>
+      <h1>Name of our teams </h1>
+
+      {details.map((data) => (
+        <div>
+             <Link href={`about/${data.id}`}>Name : {data.name}</Link>
+        </div>
+     
+      ))}
+
+    </>
   )
 }
